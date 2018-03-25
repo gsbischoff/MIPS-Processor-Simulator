8234e11bb25035416d5838a17334c42be33570dd
paul <paul.torre@richmond.edu>
2018-03-25 14:14:12 -0400

    started to implement ControlUnit header

---
+++ ControlUnit.h
@@ -0,0 +1,22 @@
+#ifndef __CONTROLUNIT_H__
+#define __CONTROLUNIT_H__
+
+class ControlUnit
+{
+    public:
+
+        //all the control lines
+        int RegDst;
+        int RegWrite;
+        int ALUSrc;
+        int PCSrc;
+        int MemRead;
+        int MemWrite;
+        int MemToReg;
+
+        ControlUnit();
+        ~ControlUnit();
+
+        void set_control_lines(); //update all the instance variables to set the data path... will be called once per instruction.
+};
+#endif
