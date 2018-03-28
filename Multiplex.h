3c3c09d686f4f1845c0b4a26f5dae300364a2060
paul <paul.torre@richmond.edu>
2018-03-27 23:28:31 -0400

    made signatures consistent

--- Multiplex.h
+++ Multiplex.h
@@ -15,10 +15,10 @@ class Multiplex
         ~Multiplex();
 
         void print_out();                 //print out the values in the multiplexor
-        void setOutput();                 //sets the output line
-        void setSelector(int selector);   //sets selector bit based on the control line
+        void set_output();                 //sets the output line
+        void set_selector(int selector);   //sets selector bit based on the control line
 
-            //multiplex1.setSelector(control.ALUSrc)
+            //multiplex1.set_selector(control.ALUSrc)
 
 };
 #endif
