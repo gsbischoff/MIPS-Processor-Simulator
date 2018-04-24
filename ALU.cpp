9441088b945ea5158041372b701c99fa8a1e6b3e
hn3gn <hn3gn@mathcs02.richmond.edu>
2018-04-23 22:41:13 -0400

    y

--- ALU.cpp
+++ ALU.cpp
@@ -1,6 +1,10 @@
-//ALU constructor
 #include "ALU.h"
 #include "stdHeader.h"
+
+//////////////////////////////////////////////////////////
+//ALU constructor with int n (which is the ALU number) passed in
+//sets the instance variables to default state
+//////////////////////////////////////////////////////////
 
 ALU::ALU(int n)
 {
@@ -14,6 +18,10 @@ ALU::ALU(int n)
 
 ALU::ALU() {}
 
+//////////////////////////////////////////////////////////
+//ALU destructor
+//////////////////////////////////////////////////////////
+
 ALU::~ALU() {}
 
 void ALU::print_out()
@@ -22,6 +30,9 @@ void ALU::print_out()
     std::cout << " ------------- " << std::endl;
     std::cout << "|    ALU " << number << "    |" << std::endl;
     std::cout << " ------------- " << std::endl;
+    //////////////////////////////////////////////////////////
+    //Prints the inputs and outputs in hexadecimal
+    //////////////////////////////////////////////////////////
     printf("Input A: 0x%x\n", in_a);
     printf("Input B: 0x%x\n", in_b);
     printf("Control code: 0x%x\n", in_b);
@@ -31,7 +42,10 @@ void ALU::print_out()
 
 }
 
-//set the output variable to the sum of A + B
+
+//////////////////////////////////////////////////////////
+//Depending on the value of the control code, will perform specificed operations
+//////////////////////////////////////////////////////////
 void ALU::execute()
 {
     if(control == 0)
@@ -45,14 +59,16 @@ void ALU::execute()
         result = in_a | in_b;
     }
 
-    else if(control == 2)  //add
+    else if(control == 2)  
     {
+        //ADD
         result = in_a + in_b;
         if (result == 0)
             zero_flag = true;
     }
-    else if(control == 6)  //subtract
+    else if(control == 6)  
     {
+        //SUBTRACT
         result = in_a - in_b;
         if(result == 0)
             zero_flag = true;
