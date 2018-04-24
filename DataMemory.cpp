265d633573ea94417c7e2d55532e154fd8232e36
hn3gn <hn3gn@mathcs02.richmond.edu>
2018-04-23 22:50:01 -0400

    comment

--- DataMemory.cpp
+++ DataMemory.cpp
@@ -1,14 +1,27 @@
 #include "DataMemory.h"
 #include "stdHeader.h"
 
+//////////////////////////////////////////////////////////
+//DataMemory constructor
+//sets the value of data memory to that of a map with unsigned 32 bit integers
+//////////////////////////////////////////////////////////
 DataMemory::DataMemory(std::map<u32, u32> data_m)
 {
   data = data_m;
 }
 
+//////////////////////////////////////////////////////////
+//DataMemory destructor
+//////////////////////////////////////////////////////////
 DataMemory::~DataMemory() {}
+//////////////////////////////////////////////////////////
+//DataMemory empty constructor
+//////////////////////////////////////////////////////////
 DataMemory::DataMemory() {}
 
+//////////////////////////////////////////////////////////
+//DataMemory print methods
+//////////////////////////////////////////////////////////
 void DataMemory::print_out()
 {
   std::cout << " ------------- " << std::endl;
@@ -20,6 +33,9 @@ void DataMemory::print_out()
   std::cout << "Control Line - MemRead: 0x" << std::hex << control_read << std::endl;
   std::cout << "Control Line - MemWrite: 0x" << std::hex << control_write << std::endl;
   std::cout << "Memory Contents..." << std::endl;
+  //////////////////////////////////////////////////////////
+  //iterates through the map and prints the hex values at each specific indice
+  //////////////////////////////////////////////////////////
   for(auto iter = data.begin(); iter != data.end(); ++iter)
   {
     std::cout << "0x" << std::hex << iter->first << ":" << iter->second << std::endl;
@@ -27,20 +43,27 @@ void DataMemory::print_out()
   std::cout << std::endl;
 }
 
+//////////////////////////////////////////////////////////
+//execute method for data memory and depending on the values of the control code will perform a read or write
+//////////////////////////////////////////////////////////
 void DataMemory::execute()
 {
     if(control_read == 1)
     {
+        //////////////////////////////////////////////////////////
         //this is a data read
         //retrieve data at this->address
         //set it to this->read_data
+        //////////////////////////////////////////////////////////
         read_data = data[address];
 
     }
     else if(control_write == 1)
     {
+        //////////////////////////////////////////////////////////
         //this is a data write
         //write the data at this->address with this->write_data
+        //////////////////////////////////////////////////////////
         data[address] = write_data;
     }
 }
